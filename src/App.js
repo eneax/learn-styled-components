07aaf8e41dbf9d500e1c678f2a9b76e012bbbd1f
eneax <19818937+eneax@users.noreply.github.com>
2019-01-02 11:18:03 +0100

    Props In Styled Components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ const Heading = styled.h1`
   font-size: 2rem;
 `;
 
+const Button = styled.button`
+  background: indigo;
+  padding: 5px 10px;
+  border-radius: 4px;
+  border: none;
+  color: white;
+  font-size: 2rem;
+
+  /* if props.type === 'cancel', then set the background to tomato */
+  ${props => props.type === 'cancel' && 'background: tomato;'}
+  /* background: ${props => (props.type === 'cancel' ? 'tomato' : 'indigo')}; */
+  /* background: ${({type}) => (type === 'cancel' ? 'tomato' : 'indigo')}; */
+`;
+
 class App extends Component {
   render() {
     return (
@@ -16,6 +30,11 @@ class App extends Component {
           <Heading>
             Edit <code>src/App.js</code> and save to reload.
           </Heading>
+
+          <Button>Save</Button>
+          <Button type="cancel">Cancel</Button>
+          
+          <Heading>Heading Two</Heading>
           <a
             className="App-link"
             href="https://reactjs.org"
