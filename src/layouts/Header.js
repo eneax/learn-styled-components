858199b36b6afe2f9a2f42a0a4aba43ecb76c5af
eneax <19818937+eneax@users.noreply.github.com>
2019-01-03 18:34:55 +0100

    Writing Good Mixins

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import logo from '../logo.svg';
-import { purple, elevation } from '../utilities';
+import { purple, elevation, fixed, serif } from '../utilities';
 
 const Header = ({ className }) => (
   <header className={className}>
     <img src={logo} alt="logo" className="logo" />
+    Hello
   </header>
 );
 
@@ -13,8 +14,12 @@ const Header = ({ className }) => (
 // styles
 export default styled(Header)`
   background: ${purple};
-  ${elevation[2]}; /* pick the third item in the array */
   padding: 10px 5%;
+  width: 100%;
+  ${serif};
+  ${fixed()};
+  ${elevation[2]}; /* pick the third item in the array */
+
   .logo {
     width: 60px;
   }
--- src/utilities/Position.js
+++ src/utilities/Position.js
@@ -1,8 +1,31 @@
 import { css } from 'styled-components';
 
 // CSS Helper --> Needed for props in mixins
-export const fixedTop = css`
+export const fixed = ({ x = 0, y = 0, yProp = 'top', xProp = 'left' } = {}) => css`
   position: fixed;
-  top: ${({ top }) => top + 'px'};
-  left: 0;
+  ${yProp}: ${y};
+  ${xProp}: ${x};
 `;
+
+export const absolute = ({
+  x = 0,
+  y = 0,
+  yProp = "top",
+  xProp = "left"
+} = {}) => css`
+  position: absolute;
+  ${yProp}: ${y};
+  ${xProp}: ${x};
+`;
+
+
+/*
+
+{ x = 0, y = 0 } = {}
+
+- the default value is an empty object
+- if we pass an object, then the default parameters are 0
+- yProp default is 'top' 
+- xProp default is 'left'
+
+*/
