9adf9ac537f64bed821dda1fee9d091b6ec1c3eb
eneax <19818937+eneax@users.noreply.github.com>
2019-01-03 11:01:18 +0100

    Organization Part 2

--- src/App.js
+++ src/App.js
@@ -1,71 +1,15 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import logo from './logo.svg';
 import GlobalStyle from './Global';
-import { Heading, Button, CancelButton } from './elements';
-
-
-// make Fake aware of the styles that will be applied to it
-const Fake = ({ className }) => (
-  <div className={className}>
-    <h2>I'm a fake component</h2>
-  </div>
-);
-
-const AppWrapper = styled.div`
-  text-align: center;
-
-  header {
-    background: teal;
-    &:hover {
-      color: gold;
-    }
-  }
-
-  /* select every Button (not CancelButton) component inside of AppWrapper */
-  ${Button} {
-    margin-bottom: 2rem;
-  }
-`;
-
-
-// DoubleFake is a styled version of Fake
-const DoubleFake = styled(Fake)`
-  h2 {
-    color: red;
-  }
-`;
+import Header from './layouts/Header';
 
 
 class App extends Component {
   render() {
     return (
-      <AppWrapper>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Heading>
-            Edit <code>src/App.js</code> and save to reload.
-          </Heading>
-
-          <Button>Save</Button>
-          <CancelButton top="100">Cancel</CancelButton>
-          
-          <Heading>Heading Two</Heading>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-
-          <DoubleFake />
-          <Fake />
-
-          <GlobalStyle />
-        </header>
-      </AppWrapper>
+      <div>
+        <Header />
+        <GlobalStyle />
+      </div>
     );
   }
 }
