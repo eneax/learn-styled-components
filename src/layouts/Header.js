9e01426d395ac562fb0fc8f57e22be0192f290ae
eneax <19818937+eneax@users.noreply.github.com>
2019-01-03 11:29:25 +0100

    Strategies For Variables

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import logo from '../logo.svg';
+import { purple, elevation } from '../utilities';
 
 const Header = ({ className }) => (
   <header className={className}>
@@ -11,7 +12,8 @@ const Header = ({ className }) => (
 
 // styles
 export default styled(Header)`
-  background: #524763;
+  background: ${purple};
+  ${elevation[2]}; /* pick the third item in the array */
   padding: 10px 5%;
   .logo {
     width: 60px;
